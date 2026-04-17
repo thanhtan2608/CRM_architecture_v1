2d490ddc8de601ecb869a9e3bc83f2ed8911df01
TruongDamCongQuy <quytruong1404@gmail.com>
2026-04-17 16:02:46 +0700

    feat: initial clean architecture template structure

---
+++ src/main/java/org/example/template_architecture/TemplateArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.example.template_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/template_architecture/TemplateArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.template_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
